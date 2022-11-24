827e05f041cd192a878b25eb34f2c51ac04da277
Hector Castro <32709707+Hcastro1515@users.noreply.github.com>
2022-11-24 06:26:50 +0000

    Completed working on FlashcardService

--- Flashy/Client/Services/FlashcardService/IFlashcardService.cs
+++ Flashy/Client/Services/FlashcardService/IFlashcardService.cs
@@ -6,5 +6,11 @@ namespace Flashy.Client.Services.FlashcardService
     {
         List<Flashcard> Flashcards { get; set; }
         Task GetFlashcards(); 
+        Task<Flashcard> GetFlashcardById(int id); 
+        Task CreateFlashCard(Flashcard card); 
+        Task RemoveFlashcardById(int id); 
+        Task RemoveAllFlashcards(); 
+        Task UpdateFlashcard(FlashcardService card); 
+
     }
 }
